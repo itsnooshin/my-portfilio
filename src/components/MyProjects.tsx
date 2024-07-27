638b1df1ec13797da5627341548d2d1dbd3467aa
itsnooshin <nooshindev@gmail.com>
2024-07-27 12:51:17 +0330

    fix the extra file

--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -21,14 +21,14 @@ export default function MyProjects() {
             My Projects
           </h2>
         </div>
-        <Link href={"/project"}>
+        <Link>
           <button className="hidden md:block bg-[#8470b4] rounded-full text-white px-4 py-2">
             View All
           </button>
         </Link>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
         {projectData.map((item) => (
           <div
             key={item.id}
@@ -67,7 +67,7 @@ export default function MyProjects() {
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
     </section>
   );
 }
